5f6b14a6956f1bd94558594be462df81291d4da1
José Trindade <trindade.jose77@gmail.com>
2021-02-24 17:35:44 +0100

    personal information and tokens cleaneup :D

--- qmessage/send_sms.py
+++ qmessage/send_sms.py
@@ -5,22 +5,20 @@ import json
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC58092df82efed4fe15957341c677d76d"
+account_sid = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # Your Auth Token from twilio.com/console
-auth_token = "0dded774b6963e031df9c79f2d357019"
+auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 client = Client(account_sid, auth_token)
 
 msg_body = """"""
 
 phone_list = [
-    "+447506921742", "+447490390963", "+447474937889"
-]
-#phone_list = {"1": "+447506921742", "2":"+447474937889", "3":"+447490389049", "4":"+447490390963"}
+    #!enter receiver phoneNumbers here
+] 
 
-quotes_file = json.load(open(r"C:\Users\Trindade\Documents\projects\py\qmessage\quotes.json"))
+quotes_file = json.load(open(r"C:\Users\userName\Documents\projects\py\qmessage\quotes.json"))
 sent = []
-#sent = json.load(open(r"C:\Users\Trindade\Documents\projects\py\qmessage\quotes.json"))
 
 
 def get_random_message(quotes, sent):
@@ -41,7 +39,7 @@ def send_sms(quotes, phone_list):
         for n in phone_list:
             message = client.messages.create(
                 to=n,
-                from_="+441667502053",
+                from_="+4400000000000", #!Replace with your phone number
                 body="\n\n" + quote + "\n" + "\n Author: " + author +
                 "\n\n From: A bad friend who happens to be good :D")
 
@@ -54,15 +52,15 @@ send_sms(quotes_file, phone_list)
 #from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-#account_sid = "AC58092df82efed4fe15957341c677d76d"
+#account_sid = "xxxxxxxxxxxxxxxxxxxxxxxx"
 # Your Auth Token from twilio.com/console
-#auth_token  = "0dded774b6963e031df9c79f2d357019"
+#auth_token  = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 #client = Client(account_sid, auth_token)
 
 #message = client.messages.create(
-#    to="+447506921742", 
-#    from_="+441667502053",
+#    to="+4475xxxxxxxxxx", 
+#    from_="+441xxxxxxxx",
 #    body="Hello from Python!")
 
 #print(message.sid)
